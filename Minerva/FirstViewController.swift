f8d2d54e4b316762a6b981ad582c1e7f3f967632
Tomas Korcak <korczis@gmail.com>
2020-04-26 19:51:10 +0200

    Initial Commit

---
+++ Minerva/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Minerva
+//
+//  Created by Tomas Korcak on 4/26/20.
+//  Copyright © 2020 Tomas Korcak. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Minerva/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Minerva
+//
+//  Created by Tomas Korcak on 4/26/20.
+//  Copyright © 2020 Tomas Korcak. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

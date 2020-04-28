e803f729110f3be4279b6b74f972f008ccd39129
Tomas Korcak <korczis@gmail.com>
2020-04-28 22:19:18 +0200

    WIP
    Signed-off-by: Tomas Korcak <korczis@gmail.com>

---
+++ Minerva Eye/Entity/Book+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Book+CoreDataClass.swift
+//  Minerva Eye
+//
+//  Created by Tomas Korcak on 4/28/20.
+//  Copyright © 2020 Tomas Korcak. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Book)
+public class Book: NSManagedObject {
+
+}
---
+++ Minerva Eye/Entity/Book+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  Book+CoreDataProperties.swift
+//  Minerva Eye
+//
+//  Created by Tomas Korcak on 4/28/20.
+//  Copyright © 2020 Tomas Korcak. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Book {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Book> {
+        return NSFetchRequest<Book>(entityName: "Book")
+    }
+
+    @NSManaged public var title: String?
+    @NSManaged public var id: UUID?
+    @NSManaged public var subtitle: String?
+    @NSManaged public var isbn: String?
+    @NSManaged public var desc: String?
+
+}

db65c5f08d929da9d66ba78aa37d346c3760cbbe
Tomas Korcak <korczis@gmail.com>
2020-05-09 01:07:22 +0200

    Add Extension Folder

---
+++ Minerva Eye/Extension/UIViewExtension.swift
@@ -0,0 +1,23 @@
+//
+//  UIViewExtension.swift
+//  Minerva Eye
+//
+//  Created by Tomas Korcak on 5/9/20.
+//  Copyright © 2020 Tomas Korcak. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func fadeIn(duration: TimeInterval = 1.0, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, options: .curveEaseIn, animations: {
+            self.alpha = 1.0
+        }, completion: completion)
+    }
+
+    func fadeOut(duration: TimeInterval = 1.0, delay: TimeInterval = 3.0, completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, options: .curveEaseIn, animations: {
+            self.alpha = 0.0
+        }, completion: completion)
+    }
+}
